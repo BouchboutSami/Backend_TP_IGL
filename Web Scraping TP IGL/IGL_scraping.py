49f8aeecc74bd44d7c481c4cf5063f7cfedad71e
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-12-26 18:31:30 +0100

    Liens recu avec web scraping manque un scroll pour recup tout le html

--- Web Scraping TP IGL/IGL_scraping.py
+++ Web Scraping TP IGL/IGL_scraping.py
@@ -1,20 +1,24 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from wilayas import *
-import requests
+import time
 
-filtre_wilaya = [1,16,24]
+filtre_wilaya = [16]
 url = "https://www.ouedkniss.com/immobilier/1?"
 for wilaya in filtre_wilaya:
   filtre= "regionIds={}-{}&".format(data_wilayas[wilaya],wilaya)
   url += filtre
-url = url[:-2]
-
-driver = webdriver.Chrome('./chromedriver')
+url = url[:-1] 
+print(url)
+driver = webdriver.Chrome("./chromedriver")
+driver.set_window_size(1920, 1080)
 driver.get(url)
-
+time.sleep(5)
 html = driver.page_source
 doc = BeautifulSoup(html,"html.parser")
-
-# WEB SCRAPING PART
+content = doc.find_all("a",{"class":"d-flex flex-column flex-grow-1 v-card v-card--link v-sheet o-announ-card-content theme--dark"})
+print(content[0])
 
